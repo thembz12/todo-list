76154ea5c56ff7972b4e2adfbaf193325c9ac124
thembz12 <oyin1239@gmail.com>
2024-07-31 14:53:39 +0100

    update

--- router/todoRouter.js
+++ router/todoRouter.js
@@ -5,10 +5,10 @@ const router = require ("express").Router()
 
 
 router.post("/create-content", authenticate ,createContent)
-router.get("/getone-content", getOneContent)
-router.get("/getall-content",getAllContent )
-router.get("/update-content",updateContent )
-router.get("/delete-content",deleteContent )
+router.get("/getone-content",authenticate, getOneContent)
+router.get("/getall-content",authenticate, getAllContent )
+router.put("/update-content",authenticate, updateContent )
+router.delete("/delete-content",authenticate , deleteContent )
 
 
 module.exports = router
